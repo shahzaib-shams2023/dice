c0b504d1f49b946c9eb69411167b8e1cf69dff53
shahzaib-shams <shahzaib@aptechgdn.net>
2024-07-24 03:09:56 +0500

    files

---
+++ index.js
@@ -0,0 +1,32 @@
+//PLayer 1
+
+var randomNumber1 = Math.floor(Math.random() * 6 ) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+//Random Img Selection
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+//Player 2
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+randomImageSource2 = "images/dice" + randomNumber2 + ".png"
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML= "🚩Player 1 wins";
+
+}
+else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩 player 2 wins";
+}else{
+    document.querySelector("h1").innerHTML = "🚩 Draw";
+}
